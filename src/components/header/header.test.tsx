b70b5be81f8efa9514b95605770c3d28606c64f0
eclopez <erik@eriklopez.net>
2025-01-22 20:39:55 -0700

    Add vitest

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from "./header";
+
+describe("Header", () => {
+  it("renders", () => {
+    render(<Header text="Hello, world!" />);
+
+    expect(screen.getByText("Hello, world!")).toBeInTheDocument();
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    passWithNoTests: true,
+    reporters: "verbose",
+    include: ["src/**/*.test.{ts,tsx}"],
+    setupFiles: ["./vitest.setup.ts"],
+  },
+});
